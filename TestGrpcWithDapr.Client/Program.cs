9e82d2c88e84fcce58a64d28d8098d327941a339
Gennadii Svichinskyi <gennadii.svichinskyi@embedit.cz>
2020-05-31 19:36:07 +0200

    init: commit test project

---
+++ TestGrpcWithDapr.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Dapr.Client;
+
+namespace TestGrpcWithDapr.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string AppId = args[0] ?? "testGrpcDaprService";
+            string MethodName = args[1] ?? "SayHello";
+            string name = args[2] ?? "Gennadii";
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var client = new DaprClientBuilder().UseJsonSerializationOptions(options).Build();
+
+            var anyReply = await client.InvokeMethodAsync<HelloRequest, HelloReply>(
+                AppId, 
+                MethodName, 
+                new HelloRequest
+                {
+                    Name = name
+                }
+            );
+
+            Console.WriteLine($"Reply : {anyReply.Message}");
+        }
+    }
+}
